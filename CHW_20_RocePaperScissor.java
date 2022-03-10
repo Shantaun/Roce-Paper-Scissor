ff14c6685e4250bc024fcd8643deab3d67d19ce8
Shantaun <shantanupatil691@gmail.com>
2022-03-10 11:11:11 +0530

    Roce Paper Scissor

---
+++ CHW_20_RocePaperScissor.java
@@ -0,0 +1,45 @@
+package com.CodeWithShantanu;
+
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class CHW_20_RocePaperScissor {
+    public static void main(String[] args) {
+        // 0 for Rock
+        //1 for paper
+        //2 for Scissor
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 0 for Rock, 1 for Paper, 2 for Scissor ");
+        int userInput = sc.nextInt();
+
+
+        Random random = new Random();
+        int computerInput = random.nextInt(3);
+
+
+        if (userInput == computerInput){
+            System.out.println("Dram");
+        }
+        else if (userInput == 0 && computerInput == 2 || userInput == 1 && computerInput == 0
+        || userInput == 2 && computerInput == 1){
+            System.out.println("You Win!");
+        }
+        else {
+            System.out.println("Computer Win!");
+        }
+    //    System.out.println("Computer choice: " + computerInput); // he aplyala sagate computer ni kay number ghetla te
+     if(computerInput == 0){
+         System.out.println("Computer choice: Rock ");
+     }
+     else if(computerInput == 1){
+         System.out.println("Computer choice: paper");
+     }
+     else if(computerInput == 2){
+         System.out.println("Computer choice: Scissor");
+     }
+    }
+
+}
